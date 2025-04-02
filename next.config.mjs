d130e5775f15830bdc391e67cb014db4b9a87549
Raphael <raphael.gabbay@gmail.com>
2025-04-02 12:38:12 +0200

    try AssetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     distDir: 'out',
     images: { unoptimized: true }, // Next.js ne supporte pas bien les images optimisées sur GitHub Pages
     basePath: '/test-actions', // Important pour que GitHub Pages gère bien les liens
+    assetPrefix: '/test-actions', // Important pour que GitHub Pages gère bien les liens
   };
 
 export default nextConfig;
