e994d8b1c50c9fee8400f17143e1c3066dc6f8c7
Raphael <raphael.gabbay@gmail.com>
2025-04-02 11:14:59 +0200

    add export to next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    distDir: 'out',
+    images: { unoptimized: true }, // Next.js ne supporte pas bien les images optimisées sur GitHub Pages
+    basePath: '/test-actions', // Important pour que GitHub Pages gère bien les liens
+  };
 
 export default nextConfig;
